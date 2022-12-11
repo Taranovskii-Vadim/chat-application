43581e90aa73818129c9a81876d1b24b783f3d4a
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-12-11 19:10:12 +0400

    add socket.io

---
+++ socket/index.js
@@ -0,0 +1,23 @@
+const io = require("socket.io")(8800, {
+  cors: { origin: "http://localhost:5173" },
+});
+
+let activeUsers = [];
+
+io.on("connection", (socket) => {
+  socket.on("newUserAdd", (id) => {
+    if (!activeUsers.some((user) => user.id === id)) {
+      activeUsers.push({ id, socketId: socket.id });
+    }
+
+    console.log("activeUsers", activeUsers);
+
+    io.emit("getUsers", activeUsers);
+  });
+
+  socket.on("disconnect", () => {
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+    console.log("user disconnected", activeUsers);
+    io.emit("getUsers", activeUsers);
+  });
+});
