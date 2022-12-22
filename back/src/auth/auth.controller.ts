d9ea5bef8fc42cc7c100d672c2a1c7bcf7017667
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-12-22 20:22:35 +0400

    add jwt strategy

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Req, Post, UseGuards } from '@nestjs/common';
+import { Controller, Req, Post, UseGuards, Get } from '@nestjs/common';
 import { Request } from 'express';
 
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('/api/auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
-  @Post('signIn')
+  @Post('/signIn')
   async login(@Req() req: Request) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Req() req: Request) {
     return req.user;
   }
 }
