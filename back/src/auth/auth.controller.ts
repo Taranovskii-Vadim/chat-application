7510df4b9dc1711d266354065edda30288d03998
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-12-22 19:58:27 +0400

    add custom guard

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Req, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('/api/auth')
 export class AuthController {
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('signIn')
   async login(@Req() req: Request) {
     return req.user;
