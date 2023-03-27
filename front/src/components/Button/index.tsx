6e41320d5e288f3bb93309b5e5c4a26c54e995a0
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-03-27 21:05:02 +0400

    add form button

--- front/src/components/Button/index.tsx
+++ front/src/components/Button/index.tsx
@@ -2,8 +2,10 @@ interface Props {
   label: string;
 }
 
-const Button = ({ label }: Props): JSX.Element => {
-  return <button>{label}</button>;
-};
+const Button = ({ label }: Props): JSX.Element => (
+  <button className="bg-indigo-500 hover:bg-indigo-600 rounded-md text-white py-2 px-4 ease-in duration-200 w-3/4">
+    {label}
+  </button>
+);
 
 export default Button;
