ff9d3a195f76fd89cb687a46f9e72a47c31a492a
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-03-29 19:19:07 +0400

    add pages file

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,10 @@
-import { observer } from "mobx-react-lite";
+import { observer } from 'mobx-react-lite';
 
-import auth from "./store/auth";
+import auth from './store/auth';
 
-import Login from "./components/Login";
+import Pages from './pages';
+import Login from './components/Login';
 
-const App = (): JSX.Element => {
-  return auth.isLogged ? <div>application</div> : <Login />;
-};
+const App = (): JSX.Element => (auth.isLogged ? <Pages /> : <Login />);
 
 export default observer(App);
---
+++ front/src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Pages = (): JSX.Element => {
+  return <div>Pages</div>;
+};
+
+export default Pages;
