aa4707b5db4cd8a99bfd820a49843a7cbc96b5fe
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-03-30 20:42:34 +0400

    refactor

--- front/src/components/ui/Button/index.tsx
+++ front/src/components/ui/Button/index.tsx
@@ -1,12 +1,9 @@
-import { DetailedHTMLProps, ButtonHTMLAttributes } from "react";
+import { DetailedHTMLProps, ButtonHTMLAttributes } from 'react';
 
-type Props = DetailedHTMLProps<
-  ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
-> & { label: string };
+type Props = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & { label: string };
 
 const Button = ({ label }: Props): JSX.Element => (
-  <button className="bg-indigo-500 hover:bg-indigo-600 rounded-md text-white py-2 px-4 ease-in duration-200 w-3/4">
+  <button className="bg-sky-500 hover:bg-sky-600 rounded-md text-white py-2 px-4 ease-in duration-200 w-3/4">
     {label}
   </button>
 );
