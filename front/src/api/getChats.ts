b1b2e482e4d32b704993ea43674bdc3d26aeab8d
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-06-17 14:09:15 +0400

    refactor

--- front/src/api/getChats.ts
+++ front/src/api/getChats.ts
@@ -16,9 +16,9 @@ class GetChats implements Route {
       ...item,
       lastMessage: lastMessage && {
         id: lastMessage.id,
-        text: lastMessage.text,
         sender: mapUserDTO(lastMessage.sender),
         createdAt: formatDate(lastMessage.createdAt),
+        text: lastMessage.text || (lastMessage.file ? 'file' : ''),
       },
       avatar:
         'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
